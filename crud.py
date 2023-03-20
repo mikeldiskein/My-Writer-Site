4d8f1e25ffeda4547c108b411a569b31e857f146
mikeldiskein <101710767+mikeldiskein@users.noreply.github.com>
2023-03-20 15:31:33 +0300

    Trying to create examples of models and launch the server

--- crud.py
+++ crud.py
@@ -1,12 +1,19 @@
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from models import Book
-from schemas import BookCreate
+from models import Book, Author
 
 
-def create_book(book_data: BookCreate, db: Session = SessionLocal()):
-    book = Book(**book_data.dict())
+def create_book(title: str, author: Author, year: int, db: Session = SessionLocal()):
+    book = Book(title=title, author=author, published_date=year)
     db.add(book)
     db.commit()
     db.refresh(book)
     return book
+
+
+def create_author(first_name: str, last_name: str, db: Session = SessionLocal()):
+    author = Author(first_name=first_name, last_name=last_name)
+    db.add(author)
+    db.commit()
+    db.refresh(author)
+    return author
